fcd40af6108ff3fd6f36d55b0a0d97fae2f8bbb2
Mr_GaoYu <Mr_GaoYu@163.com>
2020-12-23 19:35:17 +0800

    项目初始化设置

---
+++ demo/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const WebpackVueRouterPlugin = require('jiti')(__dirname)('../src/WebpackVueRouterPlugin')
+
+
+module.exports = {
+    mode: 'production',
+    context: __dirname,
+    devtool: false,
+    target: 'node',
+    entry: './index.js',
+    stats: false,
+    output: {
+        filename: './output.js',
+        path: path.join(__dirname, '/dist')
+    },
+    plugins: [
+        new WebpackVueRouterPlugin()
+    ]
+}
