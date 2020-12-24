b3f3ed75299d5c0150a5ca6c631d97a21df60043
Mr_GaoYu <Mr_GaoYu@163.com>
2020-12-24 17:03:26 +0800

    新增 demo

--- src/WebpackConventionRouting.ts
+++ src/WebpackConventionRouting.ts
@@ -16,7 +16,9 @@ export default class WebpackConventionRoutingPlugin {
 
   constructor(options:WebpackConventionRoutingOptions) {}
 
-  apply(compiler: Compiler) {}
+  apply(compiler: Compiler) {
+  
+  }
 
 
 }
