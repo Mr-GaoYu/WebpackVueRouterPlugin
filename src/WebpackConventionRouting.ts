cf9f3fe8c87a9d21bae6ef26c874dc4f5a0e5c8f
Mr_GaoYu <Mr_GaoYu@163.com>
2020-12-25 17:16:46 +0800

    增加配置

--- src/WebpackConventionRouting.ts
+++ src/WebpackConventionRouting.ts
@@ -3,11 +3,22 @@ import { hook } from "./utils/webpack";
 import { Compiler } from "webpack";
 import { join, relative } from "path";
 import glob from "glob";
+import {
+  readdirSync,
+  statSync,
+  readFileSync,
+  writeFileSync,
+  existsSync,
+} from "fs";
 
 const DEFAULTS = {
   name: "webpack",
   cwd: "./demo/src/views",
-  pattern: "**/*.vue",
+  pattern: ["**/*.vue"],
+  ignore: [],
+  singular: false,
+  watch: true,
+  output: ".demo/src/router",
 };
 
 export default class WebpackConventionRoutingPlugin {
@@ -22,9 +33,48 @@ export default class WebpackConventionRoutingPlugin {
   }
 
   apply(compiler: Compiler) {
-    console.log(glob
-      .sync(this.options.pattern, {
-        cwd: this.cwd,
-      }))
+    this.getRoutes();
+  }
+
+  getFiles(root: string) {
+    if (!existsSync(root)) return [];
+
+    return readdirSync(root).filter((file) => {
+      const absFile = join(root, file);
+      const fileStat = statSync(absFile);
+      const isDirectory = fileStat.isDirectory();
+      const isFile = fileStat.isFile();
+      if (
+        isDirectory &&
+        ["components", "component", "utils", "util"].includes(file)
+      ) {
+        return false;
+      }
+      if (file.charAt(0) === ".") return false;
+      if (file.charAt(0) === "_") return false;
+      if (/\.(test|spec|e2e)\.(j|t)sx?$/.test(file)) return false;
+      if (/\.d\.ts$/.test(file)) return false;
+
+      return true;
+    });
+  }
+
+  fileToRouteReducer() {}
+
+  getRoutes() {
+    const files = this.getFiles(join(this.cwd));
+
+    const routes = files.reduce((init, file) => {
+      const absFile = join(this.cwd, "", file);
+      const stats = statSync(absFile);
+
+      if (stats.isDirectory()) {
+      } else {
+      }
+
+      return [];
+    }, []);
+
+    console.log(files);
   }
 }
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,20 +1,42 @@
 export interface WebpackConventionRoutingOptions {
   /**
    * 名称
-   * @default 'webpack'
+   * @default webpack
    */
   name?: string;
 
   /**
    * 要在其中搜索的当前工作目录。
-   * @default 'src/view'
+   * @default src/view
    */
   cwd?: string;
 
   /**
-   * 配置匹配规则
+   * 自定义配置路由文件匹配规则
    * @default
    */
-  pattern?: string;
+  pattern?: Array<string>;
 
+  /**
+   * 自定义配置文件忽略规则，与 pattern 中 重合的规则，优先忽略
+   * @default
+   */
+  ignore?: Array<string>;
+
+  /**
+   * 配置是否启用单数模式的目录
+   * @default false
+   */
+  singular?: boolean;
+
+  /**
+   * 是否监听路由变化，当watch为false时，只生成一次路由
+   * @default true
+   */
+  watch?: boolean;
+  /**
+   * 指定输出路径，不允许设定为 src、public、pages、mock、config 等约定目录
+   * @default dist
+   */
+  output?: string;
 }
